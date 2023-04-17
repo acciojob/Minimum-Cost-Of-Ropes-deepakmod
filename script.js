5e1aaf2350d758089568527efc8aa01d14d08f11
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-17 11:03:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+let arr=inputText.value.split(",");
+			arr.forEach(function(element,index){
+				arr[index]=Number(element);
+			});
+
+			let price=[];
+			while ( arr.length > 1 ) {
+				arr.sort(function(a,b){return a-b});
+				let sum=arr[0]+arr[1];
+				price.push(sum);
+				arr.splice(0,2,sum);
+			}
+			let cost=0;
+			for (let i = 0; i < price.length; i++) {
+				cost+=price[i];
+			}
+			let div=document.getElementById("result");
+			div.innerText=cost;
