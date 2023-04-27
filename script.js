c90dcf2d337080f4871879fc70dbe73b3741d53a
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-27 16:20:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 function calculateMinCost() {
+      var inputElement = document.getElementById('rope-lengths').value;
+      var arr = inputElement.split(',');
+      arr.sort(function (a,b) {return a-b});
 
-  var inputElement = document.getElementById('rope-lengths').value;
-  var arr = inputElement.split(',');
-   arr.sort(function (a,b) {return a-b});
-
- var cost = 0;
- while (arr.length>1){
-  var res = Number(arr[0])+ Number(arr[1]);
-        arr.shift()
-        arr.shift()
-  arr.push(res);
-  cost +=res;
-
-  
-
-  arr.sort(function (a,b) {return a-b})
- }
-  //your code here
-  document.getElementById("result").innerHTML = cost
-  
-}
+      var cost = 0;
+      while (arr.length > 1){
+        var res = Number(arr[0]) + Number(arr[1]);
+        arr.shift();
+        arr.shift();
+        arr.push(res);
+        cost += res;
+        arr.sort(function (a,b) {return a-b});
+      }
+      document.getElementById("result").innerHTML = cost;
+    }
